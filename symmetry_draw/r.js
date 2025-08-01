b9a96bfcdf053d111b012e2132de675a5926da00
source21 <sho11decade@gmail.com>
2025-08-02 08:06:36 +0900

    change file name.

--- symmetry_draw/r.js
+++
@@ -1,9 +0,0 @@
-function redirect_lang() {
-    var lang = window.navigator.language;
-    if ( lang.match(/en/)) {
-        window.location.href = './index_en.html';
-    }
-}
-window.onload = function(){
-    redirect_lang();
-    }
